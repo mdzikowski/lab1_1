756f9f27ccdf8364e60967ea90b804d7c4e15e9c
Jakub Mankowski <jakub.mankowski20@gmail.com>
2018-03-12 19:20:55 +0100

    create hashCode and sameAs method for Product and Discount

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -17,4 +17,52 @@ public class Discount {
         this.money.value = money.value;
         this.money.currency = money.currency;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (discount == null ? 0 : discount.hashCode());
+        result = prime * result + (discountCause == null ? 0 : discountCause.hashCode());
+        result = prime * result + (int) money.value;
+        result = prime * result + (money.currency == null ? 0 : money.currency.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Discount other = (Discount) obj;
+        if (discount == null) {
+            if (other.discount != null) {
+                return false;
+            }
+        } else if (!discount.equals( other.discount )) {
+            return false;
+        }
+        if (discountCause == null) {
+            if (other.discountCause != null) {
+                return false;
+            }
+        } else if (!discountCause.equals( other.discountCause )) {
+            return false;
+        }
+        if (money == null) {
+            if (other.money != null) {
+                return false;
+            }
+        } else if (!money.equals( other.money )) {
+            return false;
+        }
+        return true;
+    }
+
 }
--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -3,7 +3,7 @@ package pl.com.bottega.ecommerce.sales.domain.offer;
 import java.math.BigDecimal;
 import java.util.Date;
 
-public class Product{
+public class Product {
     public String productId;
 
     public BigDecimal productPrice;
@@ -17,9 +17,79 @@ public class Product{
     public int quantity;
 
     public BigDecimal totalCost;
-
-    public String currency;
+//
+//    public String currency;
 
     public Money money;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (productName == null ? 0 : productName.hashCode());
+        result = prime * result + (productPrice == null ? 0 : productPrice.hashCode());
+        result = prime * result + (productId == null ? 0 : productId.hashCode());
+        result = prime * result + (productType == null ? 0 : productType.hashCode());
+        result = prime * result + quantity;
+        result = prime * result + (totalCost == null ? 0 : totalCost.hashCode());
+        result = prime * result + (money == null ? 0 : money.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Product other = (Product) obj;
+        if (productName == null) {
+            if (other.productName != null) {
+                return false;
+            }
+        } else if (!productName.equals( other.productName )) {
+            return false;
+        }
+        if (productPrice == null) {
+            if (other.productPrice != null) {
+                return false;
+            }
+        } else if (!productPrice.equals( other.productPrice )) {
+            return false;
+        }
+        if (productId == null) {
+            if (other.productId != null) {
+                return false;
+            }
+        } else if (!productId.equals( other.productId )) {
+            return false;
+        }
+        if (productType != other.productType) {
+            return false;
+        }
+        if (quantity != other.quantity) {
+            return false;
+        }
+        if (totalCost == null) {
+            if (other.totalCost != null) {
+                return false;
+            }
+        } else if (!totalCost.equals( other.totalCost )) {
+            return false;
+        }
+        if (money == null) {
+            if (other.money != null) {
+                return false;
+            }
+        } else if (!money.equals( other.money )) {
+            return false;
+        }
+        return true;
+    }
 }
 
