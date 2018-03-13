3862b5ec03287f669103c60596203010909cd4b1
Jakub Mankowski <jakub.mankowski20@gmail.com>
2018-03-13 13:17:42 +0100

    add money functions: hashcode and equals

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -7,5 +7,43 @@ public class Money {
 
     public String currency;
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (value == null ? 0 : value.hashCode());
+        result = prime * result + (currency == null ? 0 : currency.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Money other = (Money) obj;
+        if (value == null) {
+            if (other.value != null) {
+                return false;
+            }
+        } else if (!value.equals( other.value )) {
+            return false;
+        }
+        if (currency == null) {
+            if (other.currency != null) {
+                return false;
+            }
+        } else if (!currency.equals( other.currency )) {
+            return false;
+        }
+        return true;
+    }
+
 
 }
