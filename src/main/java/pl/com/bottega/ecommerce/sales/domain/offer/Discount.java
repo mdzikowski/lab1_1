c8fc395f49a99f8679ed1149f4596dbece20b40d
mjakmiyo <jedrzejekmonika@gmail.com>
2018-03-20 18:58:51 +0100

    Add hashCode method to Product class, Money class and Discount class

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -20,4 +20,13 @@ public class Discount {
     public BigDecimal getDiscount() {
         return discount;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (discount == null ? 0 : discount.hashCode());
+        result = prime * result + (discountCause == null ? 0 : discountCause.hashCode());
+        return result;
+    }
 }
--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -21,4 +21,13 @@ public class Money {
         return currency;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (value == null ? 0 : value.hashCode());
+        result = prime * result + (currency == null ? 0 : currency.hashCode());
+        return result;
+    }
+
 }
